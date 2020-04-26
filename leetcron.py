683077aa56e55d9074a765c0ebcfeba796c58bb7
jing1201 <low.jing@hotmail.com>
2020-04-26 07:57:57 -0400

    trying crontab

--- leetcron.py
+++ leetcron.py
@@ -1,4 +1,20 @@
 from crontab import *
+import sys
 
-cron = CronTab()
+cron = CronTab(user='jlionwg')
+
+found = False
+for j in cron.find_command(sys.executable+" leetcode_to_github.py"): #need to get full path of file too
+    found = True
+    job = j
+
+if not found:
+    job = cron.new(command=sys.executable+" leetcode_to_github.py")
+
+job.hour.on(0)
+cron.write()
+
+
+
+
 
